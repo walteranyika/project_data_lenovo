fbaa016c1912f80f85c3e190495226714425c756
Walter Indeche <walter.indeche@andela.com>
2023-11-09 08:46:58 +0300

    Fixed file names to be unique for uploaded images

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,17 @@
+import os.path
+import uuid
+
 from django.db import models
 
 
 # Create your models here.
+def generate_unique_name(instance, filename):
+    name = uuid.uuid4()  # universally unique id  pic.kenya.png ["kenya", "png"]
+    ext = filename.split(".")[-1]
+    full_filename = f"{name}.{ext}"
+    # full_filename = "%s.%s" % (name, ext)
+    return os.path.join("students", full_filename)  # students/100/xyz.png
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=25)
@@ -11,7 +21,7 @@ class Student(models.Model):
     weight = models.DecimalField(max_digits=5, decimal_places=2)  # 100.500
     is_sporty = models.BooleanField(default=False)
     kcpe_score = models.IntegerField()
-    profile_pic = models.ImageField(upload_to="students", null=True, default="students/default.png")
+    profile_pic = models.ImageField(upload_to=generate_unique_name, null=True, default="students/default.png")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
